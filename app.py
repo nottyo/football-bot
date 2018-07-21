55553637b22172ffc4fd659f6e9bb185575bf980
traitanit-h <traitanit.h@linecorp.com>
2018-07-21 22:31:10 +0700

    add main menu

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ from linebot.models import (
     UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
     FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
     TextComponent, SpacerComponent, IconComponent, ButtonComponent,
-    SeparatorComponent,
+    SeparatorComponent, CarouselContainer
 )
 
 
@@ -118,14 +118,55 @@ def get_soccersuck_news(limit):
     return jsonify(data)
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
-    text = event.message.text
+def bind_default_rich_menu(event):
     if isinstance(event.source, SourceUser):
         id = event.source.user_id
         rich_menu_list = line_bot_api.get_rich_menu_list()
         for rich_menu in rich_menu_list:
-            line_bot_api.link_rich_menu_to_user(id, rich_menu.rich_menu_id)
+            if rich_menu.chat_bar_text == 'MainMenu':
+                line_bot_api.link_rich_menu_to_user(id, rich_menu.rich_menu_id)
+
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    if event.postback.data == 'news=all':
+        carousel_template = CarouselContainer()
+        # bbc-sport
+        bbc_data = rss_feed.get_bbc_feed(5)
+        carousel_template.contents.append(football_news.get_news_bubble("#FEE63E", bbc_data))
+        # sky-sport
+        sky_data = rss_feed.get_skysports_feed(5)
+        carousel_template.contents.append(football_news.get_news_bubble("#BB0211", sky_data, header_text_color="#ffffff"))
+        # goal-com
+        goal_data = rss_feed.get_goal_feed(5)
+        carousel_template.contents.append(
+            football_news.get_news_bubble("#091F2C", goal_data, header_text_color="#ffffff"))
+        # guardian
+        guardian_data = rss_feed.get_guardian_feed(5)
+        carousel_template.contents.append(
+            football_news.get_news_bubble("#09508D", guardian_data, header_text_color="#ffffff"))
+        # mirror
+        mirror_data = rss_feed.get_mirror_feed(5)
+        carousel_template.contents.append(
+            football_news.get_news_bubble("#E80E0D", mirror_data, header_text_color="#ffffff"))
+        # shotongoal
+        shotongoal_data = rss_feed.get_shot_on_goal_feed(5)
+        carousel_template.contents.append(
+            football_news.get_news_bubble("#1A1A1A", shotongoal_data, header_text_color="#ffffff"))
+        # soccersuck
+        soccersuck_data = rss_feed.get_soccersuck_feed(5)
+        carousel_template.contents.append(
+            football_news.get_news_bubble("#197F4D", soccersuck_data))
+
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text='AllNews', contents=carousel_template))
+
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    text = event.message.text
+    result = ''
+    bind_default_rich_menu(event)
 
     if text.lower() == 'news=bbc-sport':
         data = rss_feed.get_bbc_feed(5)
@@ -151,6 +192,11 @@ def handle_text_message(event):
 
     if isinstance(result, BubbleContainer):
         line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text='news', contents=result))
+
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    bind_default_rich_menu(event)
 
 
 if __name__ == '__main__':
